acc88b3210db85a20583dfedfa68ec5fcbe974d4
Daniel <daniel.santiago2696@alumnos.udg.mx>
2020-10-12 12:53:37 -0500

    Version 1.0.0

---
+++ src/Database/DatabaseConector.java
@@ -0,0 +1,13 @@
+/*
+ * En esta sección ira solamente la conexión a la base de datos MySql
+ *Junto con sus acciones
+ */
+package Database;
+
+/**
+ *
+ * @author Shadowkiller
+ */
+public class DatabaseConector {
+    
+}
---
+++ src/Program/Server.java
@@ -0,0 +1,14 @@
+/*
+ * En esta sección irá la conexión de la base de datos y todas las operaciones
+ * Que se necesitarán a lo largo del programa
+ * Por lo que la mayoria de funciones debe ir aqui
+ */
+package Program;
+
+/**
+ *
+ * @author Shadowkiller
+ */
+public class Server {
+    
+}
